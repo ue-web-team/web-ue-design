c3cd1fa824e0cf596e2a62aca9367836e7e9181f
vejron <bjorn.yttergren@gmail.com>
2021-12-01 09:06:32 +0100

    wip

---
+++ src/components/u-forms/UFormCheckbox.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+  <div class="flex items-center">
+    <input
+      class="focus-style mr-2 u-form-control"
+      :class="{ 'border-red': !!errorMessage }"
+      type="checkbox"
+      :id="inputId"
+      :aria-describedby="errorId"
+      :aria-invalid="!!errorMessage"
+      :aria-required="required"
+      :checked="checked"
+      :value="value"
+      :name="name"
+      @blur="handleBlur"
+      @change="handleChange"
+    />
+    <label :for="inputId">{{ label }} {{ required ? '*' : '' }}</label>
+  </div>
+  <p role="alert" :id="errorId" class="text-red-400" v-show="errorMessage">{{ errorMessage }}</p>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { useField } from "vee-validate";
+import { ref } from "vue";
+import { useId } from '../../logic/use-id';
+
+const inputId = ref(`u-form-input-${useId()}`);
+const errorId = ref(`u-form-error-${useId()}`);
+
+const props = defineProps({
+  modelValue: {
+    type: null,
+  },
+  value: {
+    type: String,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+  required: {
+    type: Boolean,
+    default: false
+  }
+});
+
+const {
+  checked,
+  errorMessage,
+  handleBlur,
+  handleChange,
+} = useField(props.name, undefined, {
+  type: 'checkbox',
+  checkedValue: props.value,
+});
+</script>
--- src/parts/Forms.vue
+++ src/parts/Forms.vue
@@ -1,17 +1,17 @@
 <template>
   <article>
     <h2 class="text-3xl border-b pb-4 mb-4 font-medium">Forms & validation</h2>
-    <div class="grid grid-cols-1 md:grid-cols-2">
-      <UCard class="max-w-xs">
+    <div class="grid grid-cols-1 place-items-start md:grid-cols-2">
+      <UCard class="max-w-sm">
         <UCardHeader>
-          <UCardTitle>Form example</UCardTitle>  
+          <UCardTitle>Form example 1</UCardTitle>  
         </UCardHeader>
         <UCardContent>
           <UForm
-            class="flex flex-col flex-colgap-4"
+            class="flex flex-col flex-col gap-4"
             id="first-form"
             @submit="onSubmit"
-            :initial-values="initialFormValues"
+            :initial-values="initialFormValues1"
             :validation-schema="schema"
             @invalid-submit="onInvalidSubmit"
           >
@@ -31,10 +31,40 @@
               type="password"
             />
             <UFormText textarea label="Feedback" name="feedback" />
+            <UFormCheckbox label="I agree to the terms" name="agree" value="yes"/>
           </UForm>
         </UCardContent>
         <UCardActions>
           <UButton form="first-form" class="w-full" type="submit">Submit</UButton>
+        </UCardActions>
+      </UCard>
+
+      <UCard class="max-w-sm" :color="colors.red['100']">
+        <UCardHeader>
+          <UCardTitle>Form example 2</UCardTitle>  
+        </UCardHeader>
+        <UCardContent>
+          <UForm
+            class="flex flex-col flex-col gap-4"
+            id="second-form"
+            @submit="onSubmit"
+            :initial-values="initialFormValues2"
+            :validation-schema="schema"
+            @invalid-submit="onInvalidSubmit"
+          >
+            <UFormText
+              placeholder="name placheholder"
+              required
+              label="Enter your name"
+              name="name"
+              type="text"
+            />
+            <UFormText required label="Enter your email" name="email" type="email" />
+            <UFormCheckbox label="I agree to the terms" name="agree" value="yes"/>
+          </UForm>
+        </UCardContent>
+        <UCardActions>
+          <UButton form="second-form" class="w-full" type="submit">Submit</UButton>
         </UCardActions>
       </UCard>
     </div>
@@ -43,6 +73,7 @@
 
 <script setup lang="ts">
 import * as yup from 'yup';
+import { colors } from '@/config/colors'
 
 const onSubmit = (values: any) => {
   alert(JSON.stringify(values, null, 2));
@@ -55,8 +86,13 @@ const onInvalidSubmit = ({ values, errors, results }) => {
   console.log(results); // a detailed map of field names and their validation results
 }
 
-const initialFormValues = {
+const initialFormValues1 = {
   feedback: 'initial feddback set before',
+};
+
+const initialFormValues2 = {
+  name: 'Markus Marklund',
+  email: 'marcus.marklund@aik.se'
 };
 
 const schema = yup.object().shape({
@@ -66,6 +102,7 @@ const schema = yup.object().shape({
   confirm_password: yup.string()
     .required()
     .oneOf([yup.ref("password")], "Passwords do not match"),
+  agree: yup.string().required("Du måste gå med på våra villkor")
 });
 
 </script>
