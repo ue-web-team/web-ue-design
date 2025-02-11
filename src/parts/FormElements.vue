9177045ca442414698eb40d5c0a7b007c495b68d
Thomas Hämälä <Thomas.Hamala@umeaenergi.se>
2025-02-11 16:31:06 +0100

    Fix

--- src/parts/FormElements.vue
+++ src/parts/FormElements.vue
@@ -3,23 +3,11 @@
     <h2 class="text-3xl border-b pb-4 font-medium">Form elements</h2>
     <h3 class="text-xl my-4">Radio & checkbox</h3>
     <div class="flex flex-wrap gap-4">
-      <fieldset
-        class="border-gray-600 border-2 px-3 pt-2 pb-4 rounded-md inline-flex flex-col gap-2"
-      >
-        <legend class="px-2">Radio fieldset</legend>
-        <div class="pl-2 flex items-center gap-2">
-          <input type="radio" value="kola" v-model="radio" id="id11" />
-          <label for="id11">I want kola</label>
-        </div>
-        <div class="pl-2 flex items-center gap-2">
-          <input type="radio" value="dumle" v-model="radio" id="id22" />
-          <label for="id22">I want dumle</label>
-        </div>
-        <div class="pl-2 flex items-center gap-2">
-          <input type="radio" disabled value="suris" v-model="radio" id="id32" />
-          <label for="id32">I want disabled suris</label>
-        </div>
-      </fieldset>
+      <UFormFieldset legend="Radio fieldset">
+        <UFormRadio class="px-2" label="I want kola" value="kola" name="radio"/>
+        <UFormRadio class="px-2" label="I want dumle" value="dumle" name="radio" />
+        <UFormRadio class="px-2" label="I want suris" value="suris" name="radio"/>
+      </UFormFieldset>
       <fieldset
         class="border-gray-600 border-2 px-2 pt-2 pb-4 rounded-md inline-flex flex-col gap-2"
       >
