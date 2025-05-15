854e8123eca7f268cdf70faa1f9b937ed3e4446e
Muhammad Nauman <naumanmalik775@gmail.com>
2025-05-15 10:58:12 +0200

    added Logo

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { isDark } from './logic';
+import Logo from './parts/Logo.vue';
 </script>
 
 <template>
@@ -12,6 +13,7 @@ import { isDark } from './logic';
       </UIconButton>
     </header>
     <main class="grid grid-cols-1 bg-white rounded-xl gap-8 mb-28 p-8 dark:bg-evergreen">
+      <Logo></Logo>
       <Colors></Colors>
       <Buttons></Buttons>
       <IconButtons></IconButtons>
---
+++ src/parts/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <article>
+    <h2 class="text-3xl border-b pb-4 mb-6 font-medium bg-li">Logo</h2>
+    <ULogo class="h-10" />
+    <ULogo short class="h-10 mt-5" />
+    <ULogo class="h-10 mt-5" :color="false" />
+    <h3 class="text-xl my-4">Usage</h3>
+    <Prism language="html" class="codesnippet">{{ code }}</Prism>
+  </article>
+</template>
+
+<script setup lang="ts">
+import ULogo from '../components/u-logo/ULogo.vue';
+
+import Prism from '@/lib/code-block';
+
+const code = `
+<ULogo class="h-10"  />
+<ULogo short class="h-10"  /> set short to true for short logo -> default is false
+<ULogo :color="false" class="h-10"  /> // set color to false for white logo -> default is true
+`;
+</script>
