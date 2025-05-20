82d515d497a2ffe463db8647862310f14d0aa3c8
Muhammad Nauman <naumanmalik775@gmail.com>
2025-05-20 10:22:15 +0200

    extend tailwind theme

--- src/config/theme.ts
+++ src/config/theme.ts
@@ -1,13 +1,16 @@
 import { colors } from './colors';
 
 export const theme = {
-  colors,
   extend: {
+    colors,
     fontFamily: {
       sans: ['Borna', 'sans-serif'],
     },
     lineHeight: {
       '11': '2.75rem',
+    },
+    boxShadow: {
+      hard: '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.34)',
     },
   },
 };
